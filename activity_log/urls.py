27c8e5911575fd0b8b7852be5f8359e410cbbcce
MdMonirHossan <mdraju01820@gmail.com>
2025-08-02 20:26:26 +0600

    Working on activity log serializer, view and update settings for adding defautl pagination functionality

--- activity_log/urls.py
+++ activity_log/urls.py
@@ -1,4 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from .views import (
+    ActivityLogViewSet
+)
 
-urlpatterns = [
+# Default router initialization
+router = DefaultRouter(trailing_slash=False)
 
-]
+# Register route for ViewSets
+router.register(r'activity-logs/?', ActivityLogViewSet, basename='activity_logs_viewset')
+
+
+urlpatterns = router.urls
--- activity_log/views.py
+++ activity_log/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import ActivityLog
+from .serializers import ActivityLogSerializer
 
 # Create your views here.
+class ActivityLogViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ActivityLog.objects.all()
+    serializer_class = ActivityLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
